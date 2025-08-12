33bc2708a37acbe111ee639464d02c6948fb8744
Isabel Gamboa <gamboaa47@gmail.com>
2025-08-12 17:32:35 -0600

    Se crea funcion para agregar nombres de amigos en una cadena

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaNombresAmigos = []; // declaro la cadena
+
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById("amigo").value;
+    if (nombreAmigo.trim() === ""){
+        alert ("Ingresa un nombre valido")
+        document.querySelector("#amigo").value = ""; //posible nueva funcion
+    }else {
+        listaNombresAmigos.push(nombreAmigo);
+        console.log(listaNombresAmigos);
+        document.querySelector("#amigo").value = ""; //posible nueva funcion
+    }
+    return;
+}
